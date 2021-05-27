f0ae352273e8721c365d44cffd8fe44be1d1d09e
Duberly Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2021-05-26 20:00:50 -0500

    Initial Eclipse commit.

---
+++ wordtoshape/src/main/java/duberlyguarnizo/wordtoshape/App.java
@@ -0,0 +1,14 @@
+package duberlyguarnizo.wordtoshape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //pass
+    }
+}
